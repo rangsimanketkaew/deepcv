affd77aa2cf85725acd6c46d7a93d2e2506e0f55
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2020-12-03 09:58:32 +0100

    Update

---
+++ src/helpers/read_plumed_dat.py
@@ -0,0 +1,21 @@
+#------------------
+# Rangsiman Ketkaew
+#------------------
+
+import argparse
+import os
+import numpy as np
+
+parser = argparse.ArgumentParser(description="Read PLUMED data file (.dat, .log, .out, .txt) and save data as NumPy's compressed file (.npz)")
+parser.add_argument("--input", "-i", metavar="DATA", type=str, required=True, help="PLUMED data file")
+parser.add_argument("--key", "-k", metavar="KEYWORD", type=str, default="dat", help="Keyword name of the data array in .npz file")
+
+args = parser.parse_args()
+
+# Read file and remove first column
+dat = np.loadtxt(args.input, skiprows=1)[:,1:]
+
+# save array as npz
+out = os.path.splitext(args.input)[0] + "_" + args.key + ".npz"
+d = {args.key: dat}
+np.savez_compressed(out, **d)
