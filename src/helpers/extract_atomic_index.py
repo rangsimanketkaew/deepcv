b43e4ef81d4787057485b349cc758c4586315179
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2020-12-03 08:58:43 +0100

    Update

---
+++ src/helpers/extract_atomic_index.py
@@ -0,0 +1,19 @@
+#------------------
+# Rangsiman Ketkaew
+#------------------
+
+import argparse
+from collections import Counter
+
+parser = argparse.ArgumentParser(description="Extract index of all atoms from .xyz file")
+parser.add_argument("--xyz", "-i", metavar="XYZ", type=str, required=True, help="Cartesian coordinate file (.xyz)")
+
+args = parser.parse_args()
+
+f = open(args.xyz, "r")
+label = f.readlines()[2:]
+index = [i.split()[0] for i in label]
+index_ = list(set(index))
+for k in index_:
+    a = [str(i+1) for i, j in enumerate(index) if k == j]
+    print(k + "=" + ",".join(a))
