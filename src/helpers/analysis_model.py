98d1936231b2a83b1003c26034ab8a49e470cfe0
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2022-01-26 00:03:39 +0100

    fix bugs

--- src/helpers/analysis_model.py
+++ src/helpers/analysis_model.py
@@ -6,38 +6,87 @@ Info:
 28/11/2020 : Rangsiman Ketkaew
 """
 
+"Check, load, and show trained Model's outputs."
 
-import argparse
-from tensorflow.keras import models
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import models
+import matplotlib.pyplot as plt
+
+# Loss for custom_object
+@tf.function
+def GRMSE(y_true, y_pred):
+    """Calculate Geometric root mean-squared error (GRMSE)
+    """
+    N = y_pred.shape[1]
+    square_err = tf.math.square(tf.math.subtract(y_true, y_pred))
+    mult_sum = tf.einsum("ij,ij->", square_err, square_err)
+    return tf.math.pow(mult_sum, 1 / (2 * N))  # 2N^th root
+
+
+def get_inter_model(f, layer_name="bottleneck", sum_ori=False, sum_new=False):
+    """Load Keras model, show the summary of the model, get the intermediate layer (CV) 
+    and build a new model that will output this layer. 
+
+    Args:
+        f (str): Relative or absolute path to Keras model (.h5)
+        layer_name (str, optional): Name of layer you want to output it. Defaults to "bottleneck".
+        sum_ori (bool, optional): Show summary of original model. Defaults to False.
+        sum_new (bool, optional): Show summary of new model. Defaults to False.
+
+    Returns:
+        new_model (object): New model
+    """
+    try:
+        model = models.load_model(f)
+    except ValueError:
+        model = models.load_model(f, custom_objects={"GRMSE": GRMSE})
+    finally:
+        model = models.load_model(f, compile=False)
+
+    # Show summary of the original model to check the size of input(s)
+    if sum_ori:
+        print("------- Summary of original model -------")
+        model.summary()
+
+    new_model = models.Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
+    # alternative to the use of index: model.layers[index of layer].output
+
+    # Show summary of the new model
+    if sum_new:
+        print("------- Summary of original model -------")
+        new_model.summary()
+
+    return new_model
 
 
 if __name__ == "__main__":
-    info = "Check, load, and show trained Model's outputs."
-    parser = argparse.ArgumentParser(description=info)
-    parser.add_argument(
-        "-i", "--input", metavar="INPUT", dest="file", type=str, required=True,
-        help="Path to directory where saved model is stored, or a trained weight file, a trained bias file, an output file from DeepCV.")
-    parser.add_argument(
-        "-t", "--type", metavar="TYPE", dest="type", type=str, required=True,
-        choices=["model", "weight", "bias", "deepcv"],
-        help="Type of file. Check help message for supported types..")
+    ## Model
+    ori_model = r"c:\Users\Nutt\Desktop\gitlab\dataset\DA\output_2\model.h5"
+    new_model = get_inter_model(ori_model, layer_name="dense_2")
 
-    args = parser.parse_args()
+    # Create/define inputs , e.g. random inputs
+    input1 = np.random.randint(2, size=(10000, 15))
+    input2 = np.random.randint(180, size=(10000, 14))
+    input3 = np.random.randint(360, size=(10000, 13))
+    output = new_model.predict([input1, input2, input3])
+    print("Shape of output: " + str(output[0].shape))
 
-    print(f"Opening file: {args.file}")
-    
-    if args.type == "model":
-        model = models.load_model(args.file)
+    # Visualize output
+    plt.figure(figsize=(8, 4))
+    plt.title("Autoencoder")
+    plt.scatter(output[:10000, 0], output[:10000, 1], c=output[:5000], s=8, cmap="tab10")
+    plt.tight_layout()
+    plt.show()
 
-    elif args.type == "weight":
-        weight = np.load("output/model_weights.npz")
-        print(weight.files)
-        
-        print(weight['layer5'].shape)
-        print(weight['layer6'].shape)
-        print(weight['layer7'].shape)
-        print(weight['layer8'].shape)
-        print(weight['layer9'].shape)
-        print(weight['layer10'].shape)
+    ## Weight
+    file_weight = r"c:\Users\Nutt\Desktop\gitlab\dataset\DA\output_2\model_weights.npz"
+    weight = np.load(file_weight)
+    print(weight.files)
+    for i in weight.files:
+        print("Layer: " + str(i) + " => " + str(weight[i].shape))
 
+    ## Bias
+    file_bias = r"c:\Users\Nutt\Desktop\gitlab\dataset\DA\output_2\model_biases.npz"
+    bias = np.load(file_bias)
+    print(bias.files)
