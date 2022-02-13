3ec8522c88f56c11b4c8abbf93f3e2c9c64edbb6
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2022-02-13 21:03:47 +0100

    use parser

--- src/helpers/feature_PCA.py
+++ src/helpers/feature_PCA.py
@@ -6,23 +6,48 @@ Info:
 28/11/2020 : Rangsiman Ketkaew
 """
 
-import numpy as np
+import argparse
 import os
+import numpy as np
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-dir = os.path.dirname(__file__)
-f = os.path.join(dir, "../dataset/DA/features_DA_Prod_100000.npz")
+parser = argparse.ArgumentParser(description="Feature analysis using PCA")
+parser.add_argument(
+    "--npz",
+    "-i",
+    dest="npz",
+    metavar="INPUT.npz",
+    type=str,
+    required=True,
+    help="Input file (.npz) containing multiple arrays of descriptors",
+)
+parser.add_argument(
+    "--plot",
+    "-p",
+    dest="plot",
+    type=str,
+    default="2d",
+    choices=["2d", "3d"],
+    help="Show PCA plot. Supports both 2D and 3D plots.",
+)
 
-dat = np.load(f)
-print(dat.files)
+args = parser.parse_args()
 
+try:
+    dat = np.load(args.npz)
+    print("Data: ", dat.files)
+except:
+    print("Input file does not exist or cannot load .npz file")
+
+# Use specific keys for the array that saved by DeepCV's function
+# of descriptor calculation: dist, ang, and dih
+# However, the users can also change key
 mat = np.concatenate((dat["dist"], dat["ang"], dat["dih"]), axis=1)
-print(mat.shape)
+print("Shape of combinded array: ", mat.shape)
 
 # standardize the data
 mat_std = StandardScaler().fit_transform(mat)
@@ -52,23 +77,28 @@ plt.show()
 data_pca = pca.transform(mat_std)
 
 #### Plot the data in this projection ####
+
 # 2 components plot
-plt.figure()
-plt.scatter(data_pca[:, 0], data_pca[:, 1], marker="x", s=0.1)
-plt.xlabel("PC1")
-plt.ylabel("PC2")
-plt.title("Dihedral PCA: alanine tripeptide")
-# cbar = plt.colorbar()
-# cbar.set_label('Energy (kcal/mol)')
-plt.show()
+if args.plot == "2d":
+    plt.figure()
+    plt.scatter(data_pca[:, 0], data_pca[:, 1], marker="x", s=0.1)
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.title("Dihedral PCA: alanine tripeptide")
+    # cbar = plt.colorbar()
+    # cbar.set_label('Energy (kcal/mol)')
+    plt.show()
 
 # # 3 components plot
-# fig = plt.figure()
-# ax = Axes3D(fig)
-# p = ax.scatter(*zip(*reduced_mat), marker='o', s=0.1)
-# ax.set_xlabel('PC1')
-# ax.set_ylabel('PC2')
-# ax.set_zlabel('PC3')
-# # cbar = fig.colorbar(p)
-# # cbar.set_label('Energy (kcal/mol)')
-# plt.show()
+if args.plot == "3d":
+    from mpl_toolkits.mplot3d import Axes3D
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    p = ax.scatter(*zip(*reduced_mat), marker="o", s=0.1)
+    ax.set_xlabel("PC1")
+    ax.set_ylabel("PC2")
+    ax.set_zlabel("PC3")
+    # cbar = fig.colorbar(p)
+    # cbar.set_label('Energy (kcal/mol)')
+    plt.show()
