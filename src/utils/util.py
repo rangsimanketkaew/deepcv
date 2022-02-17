e881e0b0d309798bc0abb64a531ad1f4a6c22b98
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2022-02-18 00:41:53 +0100

    formatted

--- src/utils/util.py
+++ src/utils/util.py
@@ -55,19 +55,22 @@ def limit_gpu_growth():
     Solution is taken from https://www.tensorflow.org/guide/gpu#limiting_gpu_memory_growth
     """
     import tensorflow as tf
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+
+    gpus = tf.config.experimental.list_physical_devices("GPU")
     if gpus:
         try:
             # Currently, memory growth needs to be the same across GPUs
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            logical_gpus = tf.config.experimental.list_logical_devices("GPU")
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
 
+
 def fix_autograph_warning():
     import tensorflow as tf
+
     tf.autograph.experimental.do_not_convert
-    
+
